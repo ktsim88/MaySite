979a27f5767a23881e805bbe4b216ca76edf23d5
ktsim88 <526ktsim@frhsd.com>
2025-05-13 09:31:26 -0400

    first commit

---
+++ players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+        firstName: 'Francisco',
+        lastName: 'Lindor',
+        position: '55',
+        age: 31,
+        photo: 'https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI'
+    },
+    {
+        firstName: 'Francisco',
+        lastName: 'Lindor',
+        position: '55',
+        age: 31,
+        photo: 'https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI'
+    },
+    {
+        firstName: 'Francisco',
+        lastName: 'Lindor',
+        position: '55',
+        age: 31,
+        photo: 'https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI'
+    },
+    {
+        firstName: 'Francisco',
+        lastName: 'Lindor',
+        position: '55',
+        age: 31,
+        photo: 'https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI'
+    },
+    {
+        firstName: 'Francisco',
+        lastName: 'Lindor',
+        position: '55',
+        age: 31,
+        photo: 'https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI'
+    },
+]
