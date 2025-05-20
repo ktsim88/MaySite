d875684f493bb94ef6c6a645f7055579b4b064b0
ktsim88 <526ktsim@frhsd.com>
2025-05-20 11:54:01 -0400

    added legendary, beast, and dragon cookies

--- players.js
+++ players.js
@@ -45,4 +45,112 @@ const players = [
         photo: 'imgs/goldenCheese.webp',
         info: 'this cookie is a little rusty'
     },
+    {
+        firstName: 'Shadow Milk',
+        alt: 'shadowMilk',
+        lastName: 'Cookie',
+        position: 'Middle',
+        rarity: 'Beast',
+        photo: 'imgs/shadowMilk.webp',
+        info: 'this cookie is a little evil'
+    },
+    {
+        firstName: 'Mysic Flour',
+        alt: 'mysticFlour',
+        lastName: 'Cookie',
+        position: 'Rear',
+        rarity: 'Beast',
+        photo: 'imgs/mysticFlour.webp',
+        info: 'this cookie is a little white'
+    },
+    {
+        firstName: 'Burning Spice',
+        alt: 'burningSpice',
+        lastName: 'Cookie',
+        position: 'Front',
+        rarity: 'Beast',
+        photo: 'imgs/burningSpice.webp',
+        info: 'this cookie is scary'
+    },
+    {
+        firstName: 'Eternal Sugar',
+        alt: 'eternalSugar',
+        lastName: 'Cookie',
+        position: 'Middle',
+        rarity: 'Beast',
+        photo: 'imgs/eternalSugar.webp',
+        info: 'this cookie is a little pink'
+    },
+    {
+        firstName: 'Pitaya Dragon',
+        alt: 'pitayaDragon',
+        lastName: 'Cookie',
+        position: 'Middle',
+        rarity: 'Dragon',
+        photo: 'imgs/pitayaDragon.webp',
+        info: 'this cookie is a little dragon'
+    },
+    {
+        firstName: 'Sea Fairy',
+        alt: 'seaFairy',
+        lastName: 'Cookie',
+        position: 'Middle',
+        rarity: 'Legendary',
+        photo: 'imgs/seaFairy.webp',
+        info: 'this cookie is a little fairy'
+    },
+    {
+        firstName: 'Frost Queen',
+        alt: 'frostQueen',
+        lastName: 'Cookie',
+        position: 'Middle',
+        rarity: 'Legendary',
+        photo: 'imgs/frostQueen.webp',
+        info: 'this cookie is ELSA'
+    },
+    {
+        firstName: 'Black Pearl',
+        alt: 'blackPearl',
+        lastName: 'Cookie',
+        position: 'Middle',
+        rarity: 'Legendary',
+        photo: 'imgs/blackPearl.webp',
+        info: 'this cookie is a little mermaid'
+    },
+    {
+        firstName: 'Moonlight',
+        alt: 'moonlight',
+        lastName: 'Cookie',
+        position: 'Middle',
+        rarity: 'Legendary',
+        photo: 'imgs/moonlight.webp',
+        info: 'this cookie is a little moon'
+    },
+    {
+        firstName: 'Stormbringer',
+        alt: 'stormbringer',
+        lastName: 'Cookie',
+        position: 'Front',
+        rarity: 'Legendary',
+        photo: 'imgs/stormbringer.webp',
+        info: 'this cookie is something'
+    },
+    {
+        firstName: 'Wind Archer',
+        alt: 'windArcher',
+        lastName: 'Cookie',
+        position: 'Rear',
+        rarity: 'Legendary',
+        photo: 'imgs/windArcher.webp',
+        info: 'this cookie is a little archer'
+    },
+    {
+        firstName: 'Fire Spirit',
+        alt: 'fireSpirit',
+        lastName: 'Cookie',
+        position: 'Middle',
+        rarity: 'Legendary',
+        photo: 'imgs/fireSpirit.webp',
+        info: 'this cookie is a little firey'
+    },
 ]
