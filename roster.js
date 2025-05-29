50e3c6bcb99a44d655a8dc5c6e280b2c7228b27f
ktsim88 <526ktsim@frhsd.com>
2025-05-29 15:55:39 +0000

    alomost done decorating

--- roster.js
+++ roster.js
@@ -18,25 +18,26 @@ document.addEventListener('DOMContentLoaded', () => {
                         </div>
                 <div id="cookieType">${p.type}</div>
                         <p class="small text-muted mb-2">${p.position}</p>
-                            <button type="button" class="btn btn-primary show-info-btn" data-player-index="${index}" data-bs-toggle="modal" data-bs-target="#${p.alt}Modal" onclick="learnMore()">
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#${p.alt}Modal" onclick="learnMore()">
                             Learn More
                             </button>
+
 
                             <!-- Modal -->
                             <div class="modal modal-xl fade" id="${p.alt}Modal" tabindex="-1" aria-labelledby="cookieLabel" aria-hidden="true">
                             <div class="modal-dialog modal-dialog-centered">
                                 <div class="modal-content">
                                 <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="cookieLabel">'s Story</h1>
+                                    <h1 class="modal-title fs-5" id="cookieLabel">${p.firstName} ${p.lastName}'s Story</h1>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body" id="info-body">
                                     <div class="row">
-                                    <div id="cookieInfo"></div>
+                                    <div id="cookieInfo">${p.info}</div>
                                     </div>
                                 </div>
                                 <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-secondary closeBtn" data-bs-dismiss="modal">Close</button>
                                 </div>
                                 </div>
                             </div>
@@ -52,20 +53,10 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     
     render(players)
-    grid.addEventListener('click', function (e) {
-  if (e.target.classList.contains('show-info-btn')) {
-    const index = e.target.getAttribute('data-player-index')
-    const player = players[index]
-    showPlayerModal(player)
-  }
-})
+    
 
     
 })
-function learnMore() {
-    let cookieLabel = document.getElementById('cookieLabel')
-    let cookieInfo = document.getElementById('cookieInfo')
-    cookieLabel.textContent = `${this.firstName} ${this.lastName}'s Story`
-    cookieInfo.textContent =   `${this.info}`
-}
+
+
 
